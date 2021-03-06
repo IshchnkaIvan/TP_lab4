a2606284dd52cbd16adf7a92873b3bb536040bc0
Ivan Ishchanka <ivan.ishchanka@gmail.com>
2021-03-06 19:56:44 +0300

    all queries added

--- create_table.sql
+++ create_table.sql
@@ -3,30 +3,24 @@ use Enrolle_db;
 create table if not exists Enrolle
 (
 	id 				int 		unsigned auto_increment,
-    surname 		varchar(30) NOT NULL,
-    `name` 			varchar(20)	NOT NULL,
+    surname 		varchar(30),
+    `name` 			varchar(20),
     middle_name 	varchar(20),
     gender 			varchar(1),
     nationality 	varchar(30),
     birthdate 		date,
     home_address 	varchar(256),
-    CT_rating 		smallint 	unsigned NOT NULL,
-    passing_score 	smallint 	unsigned NOT NULL,
+    CT_rating 		smallint 	unsigned,
+    passing_score 	smallint 	unsigned,
     primary key (id)
 );
-
-
 create table if not exists University
 (
 	id 				int 		unsigned auto_increment,
-    university_name		varchar(80) NOT NULL,
+    university_name		varchar(80),
     university_description		varchar(200),
     primary key (id)
 );
-truncate table University;
-insert into University (university_name)
-	values("BSU");
-
 
 truncate table Enrolle;
 insert into Enrolle (surname, `name`, middle_name, gender, nationality, birthdate, home_address, CT_rating, passing_score)
@@ -36,3 +30,4 @@ insert into Enrolle (surname, `name`, middle_name, gender, nationality, birthdat
 			18/03/2002,"246000,Belarus,Gomel,Portovaya street,51",364,364),
             ("Korolko","Olga","Yurievna","F","belarussian",
 			23/10/2001,"246000,Belarus,Pinsk,Krasnoflotskaya 8,2",350,328);
+            
--- queries.sql
+++ queries.sql
@@ -3,12 +3,31 @@ USE enrolle_db;
 SELECT * FROM enrolle
 WHERE CT_rating > 225;
 
-ALTER TABLE enrolle
-ADD university_id INT;
+-- ALTER TABLE Enrolle
+-- ADD university_id INT UNSIGNED;
 
-ALTER TABLE enrolle
-ADD FOREIGN KEY (university_id) REFERENCES university(id);
+-- alter table Enrolle
+-- add FOREIGN KEY (university_id) REFERENCES university(id);
+
+select enrolle.id, enrolle.surname, enrolle.`name`, enrolle.middle_name, enrolle.birthdate, university.university_name
+from enrolle 
+left join university ON enrolle.university_id = university.id;
 
 select count(*)
-from Enrolle
+from Enrolle 
 where passing_score>250;
+
+
+
+select sum(CT_rating)
+from Enrolle
+where gender="M";
+
+select max(CT_rating),min(CT_rating)
+from Enrolle;
+
+select Enrolle.*,University.*
+from Enrolle
+inner join University
+on Enrolle.university_id=University.id
+where University.id=3;    
