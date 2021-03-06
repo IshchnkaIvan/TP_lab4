d669ecabd4d6a9c741836dbd877e01023d3cea11
mikabele <62202135+mikabele@users.noreply.github.com>
2021-03-06 14:24:24 +0300

    add for key

--- queries.sql
+++ queries.sql
@@ -3,9 +3,11 @@ USE enrolle_db;
 SELECT * FROM enrolle
 WHERE CT_rating > 225;
 
-ALTER TABLE enrolle
-ADD university_id INT 
-FOREIGN KEY (university) REFERENCES university(id);
+ALTER TABLE Enrolle
+ADD university_id INT UNSIGNED;
+
+alter table Enrolle
+add FOREIGN KEY (university_id) REFERENCES university(id);
 
 select count(*)
 from Enrolle
