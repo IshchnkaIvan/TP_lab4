b70f4c5082ebc07402a15b73448b195602ae1827
mikabele <62202135+mikabele@users.noreply.github.com>
2021-03-06 14:04:25 +0300

    add queries

--- create_table.sql
+++ create_table.sql
@@ -15,8 +15,18 @@ create table if not exists Enrolle
     primary key (id)
 );
 
+truncate table Enrolle;
 insert into Enrolle (surname, `name`, middle_name, gender, nationality, birthdate, home_address, CT_rating, passing_score)
 	values("Belevich","Mikhail","Andreevich","M","belarussian",
 			01/24/2002,"246000,Belarus,Gomel,International street,15,7",382,364),
             ("Ishchenko","Ivan","Sergeevich","M","belarussian",
-			18/03/2002,"246000,Belarus,Gomel,Portovaya street,51",364,364)
+			18/03/2002,"246000,Belarus,Gomel,Portovaya street,51",364,364),
+            ("Korolko","Olga","Yurievna","F","belarussian",
+			23/10/2001,"246000,Belarus,Pinsk,Krasnoflotskaya 8,2",350,328);
+            
+select count(*)
+from Enrolle 
+where passing_score>250;
+            
+
+
