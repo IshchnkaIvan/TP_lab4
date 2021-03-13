16695802068da770bef05c0818daa5ac324c8458
Ivan Ishchanka <ivan.ishchanka@gmail.com>
2021-03-13 14:47:03 +0300

    Class SQL_executor added

--- db_connector.py
+++ db_connector.py
@@ -7,23 +7,23 @@ class sql_executor:
     def __init__(self, configs_file_name: str) -> None:
         self.configs_file_name = configs_file_name
 
-    def __db_connect(configs_file_name: str) -> None:
+    def __db_connect(self) -> None:
         config = configparser.ConfigParser()
-        config.read('configs.txt')
+        config.read(self.configs_file_name)
         conn = mysql.connector.connect(host=config['db_connect']['host'],
                                        database=config['db_connect']['database'],
                                        user=config['db_connect']['user'],
                                        password=config['db_connect']['password'])
         return conn
 
-    def execute_sql_script(self, sql_file_name: str) -> None:
+    def execute_sql_script(self, query: str) -> None:
         try:
-            conn = self.__db_connect(self.configs_file_name)
+            conn = self.__db_connect()
             cursor = conn.cursor()
-            with open(sql_file_name) as sql_file:
-                for _ in cursor.execute(sql_file.read(), multi=True):
-                    pass
+            cursor.execute(query)
+            results_of_query = cursor.fetchall()
             conn.commit()
+            return results_of_query
         except Exception as e:
             print(e)
 
